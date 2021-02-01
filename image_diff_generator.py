31b9afe368f8999e47cd679a25c20e6288af2b7b
Alon <alonmln@gmail.com>
2021-02-01 12:51:23 +0200

    Aded title and twitter handle

--- image_diff_generator.py
+++ image_diff_generator.py
@@ -1,6 +1,5 @@
 import os
 import hashlib
-import logging
 import time
 
 from PIL import Image
@@ -12,31 +11,50 @@ from html_utils import strip_html
 PHANTOMJS_PATH = os.environ['PHANTOMJS_PATH']
 
 
-def generate_image_diff(old: str, new: str):
+def generate_image_diff(old: str, new: str, text_to_tweet: str):
     stripped_old = strip_html(old)
     stripped_new = strip_html(new)
     new_hash = hashlib.sha224(stripped_new.encode('utf8')).hexdigest()
-    logging.info(html_diff(stripped_old, stripped_new))
-    html = """
+    diff_html = html_diff(stripped_old, stripped_new)
+    html = f"""
     <!doctype html>
     <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="./css/styles.css">
-      </head>
-      <body>
-      <p>
-      {}
-      </p>
-      </body>
+
+    <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="./css/styles.css">
+    </head>
+
+    <body style="width: 500px;">
+      <div id="wrapper">
+        <div>
+          {text_to_tweet}:
+        </div>
+        <p>
+          {diff_html}
+        </p>
+        <div>
+            <p class="alignleft">
+              <img src="img/twitter.png" width="30">
+              @ILNewsDiff
+              <span class="alignright">
+                כותרת בשינוי אדרת
+              </span>
+            </p>
+        </div>
+      </div>
+    </body>
+
     </html>
-    """.format(html_diff(old, new))
+    """
     with open('tmp.html', 'w', encoding="utf-8") as f:
         f.write(html)
     driver = webdriver.PhantomJS(
         executable_path=PHANTOMJS_PATH)
+
     driver.get('tmp.html')
-    e = driver.find_element_by_xpath('//p')
+
+    e = driver.find_element_by_id('wrapper')
     start_height = e.location['y']
     block_height = e.size['height']
     end_height = start_height
@@ -44,7 +62,7 @@ def generate_image_diff(old: str, new: str):
     block_width = e.size['width']
     end_width = start_width
     total_height = start_height + block_height + end_height
-    total_width = start_width + block_width + end_width
+    total_width = 510  # Override because body width is set to 500
     timestamp = str(int(time.time()))
     driver.save_screenshot('./tmp.png')
     img = Image.open('./tmp.png')
@@ -66,3 +84,4 @@ def generate_image_diff(old: str, new: str):
     saved_file_path = f'./output/{filename}.png'
     background.save(saved_file_path)
     return saved_file_path
+
