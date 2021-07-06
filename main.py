68472589669c00fc6c77c1c90d7cdd451d74d2f8
db29 <dor295@ex.com>
2021-07-06 23:26:13 +0300

    Moved logging into a file

--- main.py
+++ main.py
@@ -1,55 +1,29 @@
 #!/usr/bin/python3
 
 import logging
-import os
-import sys
-
 from pytz import timezone
 
 from parsers.haaretz_parser import HaaretzParser
 from parsers.israel_hayom_parser import IsraelHayomParser
 from parsers.walla_parser import WallaParser
+from loggers import setup_loggers
 
 TIMEZONE = 'Israel'
 LOCAL_TZ = timezone(TIMEZONE)
-
-if 'LOG_FOLDER' in os.environ:
-    LOG_FOLDER = os.environ['LOG_FOLDER']
-else:
-    LOG_FOLDER = ''
+PARSER_CLASSES = [HaaretzParser, IsraelHayomParser, WallaParser]
 
 
 def main():
-    # logging
-    logging_filehandler = logging.FileHandler(filename=LOG_FOLDER + 'titlediff.log',
-                                              encoding='utf-8', mode='a+')
-    handlers = [logging_filehandler, logging.StreamHandler(sys.stdout)]
-    logging.basicConfig(handlers=handlers,
-                        format='%(asctime)s %(name)13s %(levelname)8s: %(message)s',
-                        level=logging.DEBUG)
-
-    logging.getLogger("requests").setLevel(logging.WARNING)
-    logging.getLogger("selenium").setLevel(logging.WARNING)
-    logging.getLogger("PIL").setLevel(logging.WARNING)
-    logging.getLogger("requests_oauthlib").setLevel(logging.WARNING)
-    logging.getLogger("chardet").setLevel(logging.WARNING)
-    logging.getLogger("tweepy").setLevel(logging.WARNING)
-    logging.getLogger("oauthlib").setLevel(logging.WARNING)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-
+    setup_loggers()
     logging.info('Starting script')
 
-    try:
-        logging.debug('Starting Parsers')
-        parsers = [HaaretzParser(LOCAL_TZ), IsraelHayomParser(LOCAL_TZ), WallaParser(LOCAL_TZ)]
-        for parser in parsers:
-            logging.info(f"Parsing {parser.get_source()}")
-            parser.parse()
-        logging.debug('Finished')
-    except Exception:
-        logging.exception('Parser')
-
-    logging.info('Finished script')
+    logging.debug('Starting Parsers')
+    parsers = [parser_class(LOCAL_TZ) for parser_class in PARSER_CLASSES]
+    for parser in parsers:
+        logging.info(f"Parsing {parser.get_source()}")
+        parser.parse()
+    
+    logging.debug('Finished')
 
 
 if __name__ == "__main__":
