17ff3bb31e7d5da63ead2bba63c2a3be888a2e0f
db29 <dor295@ex.com>
2021-07-10 16:55:07 +0300

    Get database file as an argument

--- process_data/process_data.py
+++ process_data/process_data.py
@@ -1,10 +1,17 @@
+import argparse
 
 
-from main import main
+def process_data(databse_file):
+    pass
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Process some data.')
+    parser.add_argument('database', help='A path to a database')
+
+    args = parser.parse_args()
+    process_data(args.database)
+
 
 if __name__ == "__main__":
     main()
