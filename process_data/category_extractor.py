e211054dce7ef4c7cab81da7baa3875bc99c1117
Ron Friedman <ronfri48@gmail.com>
2021-07-24 14:11:55 +0300

    Add category and type extractor

--- process_data/category_extractor.py
+++ process_data/category_extractor.py
@@ -9,16 +9,17 @@ class CategoryExtractor(object):
     @staticmethod
     def extract_article_category(current_version_data) -> str:
         article_source = current_version_data['article_source']
+        article_url = current_version_data['url']
         article_id = current_version_data['article_id']
 
         if 'Haaretz' == article_source:
-            # There are many cases, handle them 
+            # There are many cases, handle them
             # Structure is https://www.haaretz.co.il/family/askingforafriend/BLOG-1.9467825
             # https://www.haaretz.co.il/blogs/sherenf/BLOG-1.9497746
-            return article_id.split('/')[-3]
+            return ' '.join(article_url.split('/')[3:-1])
         elif 'Walla' == article_source:
             # Structure is https://news.walla.co.il/item/3415909
-            return article_id.split('/')[-1]
+            return article_url.split('/')[2].split('.')[0]
         elif 'IsraelHayom' == article_source:
             # NEXT_VERSION: Maybe we can use BERT to get category or search for key words
             return 'Unknown'
@@ -33,14 +34,16 @@ class CategoryExtractor(object):
         article_id = current_version_data['article_id']
 
         if 'Haaretz' == article_source:
-            # Structure is
-            return ''.join(str(article_id).split('.'))
+            # Structure is https://www.haaretz.co.il/news/world/.premium-LIVE-1.9481685 and
+            # without prefix to id in regular items
+            article_identifiers = article_url.split('/')[-1].split('-')[:-1]
+            return ' '.join(article_identifiers) if len(article_identifiers) > 0 else 'item'
         elif 'Walla' == article_source:
-            # Structure is
-            return article_id.split('/')[-1]
+            # Structure is https://news.walla.co.il/item/3415909
+            return article_url.split('/')[-2]
         elif 'IsraelHayom' == article_source:
             # Structure is https://www.israelhayom.co.il/opinion/846019
-            return article_id.split('/')[-2]
+            return article_url.split('/')[-2]
 
         raise Exception(f"Unknown article source {article_source} for article {article_id},"
                         f" with id {current_version_data['id']}")
